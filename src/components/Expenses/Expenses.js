d121ed09bc4adcac10bf582b2b854e087f8b8e6a
pfellow <pfellow@gmail.com>
2023-04-09 17:01:05 +1200

    initinal (expenses static cards completed)

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const { data } = props;
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={data[0].title}
+        amount={data[0].amount}
+        date={data[0].date}
+      />
+      <ExpenseItem
+        title={data[1].title}
+        amount={data[1].amount}
+        date={data[1].date}
+      />
+      <ExpenseItem
+        title={data[2].title}
+        amount={data[2].amount}
+        date={data[2].date}
+      />
+      <ExpenseItem
+        title={data[3].title}
+        amount={data[3].amount}
+        date={data[3].date}
+      />
+    </Card>
+  );
+};
+
+export default Expenses;
